b8953af4fdda96c45f6b598c3091fdab72df5956
cfrainay <clement.frainay@toulouse.inra.fr>
2017-04-14 11:12:14 +0200

    Steiner Tree Computation : use Kruskal instead of Prim
    All to handle disconnected graph, return in that case a maximum spanning forest

--- met4j-graph/src/main/java/fr/inra/toulouse/metexplore/met4j_graph/computation/algo/SteinerTreeApprox.java
+++ met4j-graph/src/main/java/fr/inra/toulouse/metexplore/met4j_graph/computation/algo/SteinerTreeApprox.java
@@ -36,6 +36,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.jgrapht.alg.KruskalMinimumSpanningTree;
 import org.jgrapht.alg.PrimMinimumSpanningTree;
 import org.jgrapht.graph.DirectedWeightedMultigraph;
 
@@ -97,8 +98,8 @@ public class SteinerTreeApprox<V extends BioEntity, E extends Edge<V>, G extends
 		
 		ArrayList<E> list = new ArrayList<E>();
 		CompressedGraph<V, E, G> cg = (new ShortestPath<V,E,G>(g)).getMetricClosureGraph(terminal, terminal, weighted);
-		PrimMinimumSpanningTree<V, PathEdge<V,E>> prim = new PrimMinimumSpanningTree<V, PathEdge<V,E>>(cg);
-		Set<PathEdge<V,E>> mst = prim.getMinimumSpanningTreeEdgeSet();
+		KruskalMinimumSpanningTree<V, PathEdge<V,E>> kruskal = new KruskalMinimumSpanningTree<V, PathEdge<V,E>>(cg);
+		Set<PathEdge<V,E>> mst = kruskal.getMinimumSpanningTreeEdgeSet();
 		for(PathEdge<V,E> edge : mst){
 			list.addAll(edge.getPath().getEdgeList());
 		}
@@ -133,39 +134,12 @@ public class SteinerTreeApprox<V extends BioEntity, E extends Edge<V>, G extends
 		
 		ArrayList<E> list = new ArrayList<E>();
 		DirectedWeightedMultigraph<V, PathEdge<V,E>> cg = (new ShortestPath<V,E,G>(g)).getMetricClosureGraph(startNodes, endNodes, weighted);
-		PrimMinimumSpanningTree<V, PathEdge<V,E>> prim = new PrimMinimumSpanningTree<V, PathEdge<V,E>>(cg);
-		Set<PathEdge<V,E>> mst = prim.getMinimumSpanningTreeEdgeSet();
+		KruskalMinimumSpanningTree<V, PathEdge<V,E>> kruskal = new KruskalMinimumSpanningTree<V, PathEdge<V,E>>(cg);
+		Set<PathEdge<V,E>> mst = kruskal.getMinimumSpanningTreeEdgeSet();
 		for(PathEdge<V,E> edge : mst){
 			list.addAll(edge.getPath().getEdgeList());
 		}
 		return list;
 	}
-	
-//	/**
-//	 * Gets the steiner tree.
-//	 *
-//	 * @param g the graph
-//	 * @param nodeOfInterest the node of interest list
-//	 * @param weighted if the graph is weighted
-//	 * @return the steiner tree
-//	 */
-//	public G getSteinerTree(Set<V> nodeOfInterest, boolean weighted){
-//		return(g.extractSubGraphFromEdgeList(getSteinerTreeList(nodeOfInterest, weighted)));
-//	}
-//	
-//	/**
-//	 * Gets the steiner tree.
-//	 *
-//	 * @param g the graph
-//	 * @param startNodes the list of source nodes
-//	 * @param endNodes  the list of target nodes
-//	 * @param weighted if the graph is weighted
-//	 * @return the steiner tree
-//	 */
-//	public G getSteinerTree(Set<V> startNodes,Set<V> endNodes, boolean weighted){
-//		return(g.extractSubGraphFromEdgeList(getSteinerTreeList(startNodes, endNodes, weighted)));
-//	}
-
-
 
 }

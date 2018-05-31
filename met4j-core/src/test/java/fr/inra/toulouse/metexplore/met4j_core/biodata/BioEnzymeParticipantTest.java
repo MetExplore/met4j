239a01e12bbdac374d7822c9a1d82d24a90c8dd9
Unknown <ludovic.cottret@inra.fr>
2018-05-31 09:28:04 +0200

    new test class for BioEnzymeParticipant

---
+++ met4j-core/src/test/java/fr/inra/toulouse/metexplore/met4j_core/biodata/BioEnzymeParticipantTest.java
@@ -0,0 +1,27 @@
+package fr.inra.toulouse.metexplore.met4j_core.biodata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+
+public class BioEnzymeParticipantTest {
+	
+
+	@Test
+	public void testToString() {
+		
+		fail("not implemented");
+		
+	}
+
+
+	@Test
+	public void testEquality() {
+		fail("not implemented");
+	}
+
+
+}

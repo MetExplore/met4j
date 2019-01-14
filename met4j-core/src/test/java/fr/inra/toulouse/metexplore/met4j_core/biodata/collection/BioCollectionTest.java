dc69fe05d3d617e060832254eeb906901801e80f
Unknown <ludovic.cottret@toulouse.inra.fr>
2019-01-14 13:41:19 +0100

    BioCollection tests (not implemented for the moment)

---
+++ met4j-core/src/test/java/fr/inra/toulouse/metexplore/met4j_core/biodata/collection/BioCollectionTest.java
@@ -0,0 +1,94 @@
+package fr.inra.toulouse.metexplore.met4j_core.biodata.collection;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BioCollectionTest {
+
+	@Test
+	public void testContainsId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEntityFromId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIds() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEntitiesFromName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRetainAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContains() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetView() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}

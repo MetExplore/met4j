938454c2673b9404b0e74177969b975656c04ae9
Unknown <ludovic.cottret@toulouse.inra.fr>
2019-01-18 16:21:40 +0100

    Test for BioAnnotation

---
+++ met4j-io/src/test/java/fr/inra/toulouse/metexplore/met4j_io/jsbml/attributes/BioAnnotationTest.java
@@ -0,0 +1,16 @@
+package fr.inra.toulouse.metexplore.met4j_io.jsbml.attributes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BioAnnotationTest {
+
+	@Test
+	public void testBioAnnotation() {
+		
+		// TODO : must throw an exception when the xml is not valid
+		fail("Not yet implemented");
+	}
+
+}

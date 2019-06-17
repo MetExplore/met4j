dd0bd024389b2c1892de7ae529f169b0cf01e8d8
Unknown <ludovic.cottret@toulouse.inra.fr>
2019-06-17 13:08:15 +0200

    New class for storing annotator comments

---
+++ met4j-io/src/main/java/fr/inra/toulouse/metexplore/met4j_io/metexplorexml/annotations/AnnotatorComment.java
@@ -0,0 +1,15 @@
+package fr.inra.toulouse.metexplore.met4j_io.metexplorexml.annotations;
+
+public class AnnotatorComment {
+	
+	public String comment;
+	public String annotator;
+	
+	public AnnotatorComment(String comment, String annotator) {
+		super();
+		this.comment = comment;
+		this.annotator = annotator;
+	}
+	
+
+}

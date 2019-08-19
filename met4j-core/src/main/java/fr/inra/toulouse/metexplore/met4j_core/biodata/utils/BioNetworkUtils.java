7f0a36c7290f88dff2e490c1d390b441e1275280
Unknown <ludovic.cottret@toulouse.inra.fr>
2019-08-19 16:40:28 +0200

    Implements and tests getChokeReactions

--- met4j-core/src/main/java/fr/inra/toulouse/metexplore/met4j_core/biodata/utils/BioNetworkUtils.java
+++ met4j-core/src/main/java/fr/inra/toulouse/metexplore/met4j_core/biodata/utils/BioNetworkUtils.java
@@ -1,7 +1,9 @@
 package fr.inra.toulouse.metexplore.met4j_core.biodata.utils;
 
+import fr.inra.toulouse.metexplore.met4j_core.biodata.BioCompartment;
 import fr.inra.toulouse.metexplore.met4j_core.biodata.BioMetabolite;
 import fr.inra.toulouse.metexplore.met4j_core.biodata.BioNetwork;
+import fr.inra.toulouse.metexplore.met4j_core.biodata.BioReactant;
 import fr.inra.toulouse.metexplore.met4j_core.biodata.BioReaction;
 import fr.inra.toulouse.metexplore.met4j_core.biodata.collection.BioCollection;
 
@@ -9,6 +11,7 @@ public class BioNetworkUtils {
 
 	/**
 	 * Return choke reactions
+	 * 
 	 * @param network
 	 * @return
 	 */
@@ -21,25 +24,35 @@ public class BioNetworkUtils {
 			BioCollection<BioMetabolite> metabolites = network.getLefts(r);
 			metabolites.addAll(network.getRights(r));
 
-			for(BioMetabolite m : metabolites) {
-			
+			for (BioMetabolite m : metabolites) {
+
 				BioCollection<BioReaction> reactionsAsSubstrate = network.getReactionsFromSubstrate(m);
 				BioCollection<BioReaction> reactionsAsProduct = network.getReactionsFromProduct(m);
-				
+
 				reactionsAsSubstrate.remove(r);
 				reactionsAsProduct.remove(r);
-				
-				if(reactionsAsSubstrate.size() == 0 || reactionsAsProduct.size() == 0)
-				{
+
+				if (reactionsAsSubstrate.size() == 0 || reactionsAsProduct.size() == 0) {
 					chokeReactions.add(r);
 					break;
 				}
-				
+
 			}
 		}
 
 		return chokeReactions;
 
 	}
+
+//	public static BioNetwork getSubNetwork(BioNetwork originalNetwork, BioCollection<BioReaction> reactions,
+//			BioCollection<BioMetabolite> additionalMetabolites) {
+//
+//		BioNetwork network = new BioNetwork();
+//
+//		BioCollection<BioMetabolite> metabolitesToAdd = originalNetwork.getMetabolitesFromReactions(reactions);
+//
+//		return network;
+//
+//	}
 
 }

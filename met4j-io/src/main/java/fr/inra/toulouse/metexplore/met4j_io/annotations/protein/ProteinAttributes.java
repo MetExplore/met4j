9a5798c63a62c2e12849e5a7dfe0f7c1e4ba8a35
Unknown <ludovic.cottret@toulouse.inra.fr>
2019-08-30 09:56:59 +0200

    Add ProteinAttributes class

---
+++ met4j-io/src/main/java/fr/inra/toulouse/metexplore/met4j_io/annotations/protein/ProteinAttributes.java
@@ -0,0 +1,7 @@
+package fr.inra.toulouse.metexplore.met4j_io.annotations.protein;
+
+import fr.inra.toulouse.metexplore.met4j_io.annotations.GenericAttributes;
+
+public class ProteinAttributes extends GenericAttributes {
+
+}

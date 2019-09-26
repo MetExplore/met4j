b0bb6cbea976cf0b343aff645ab5567953405c92
Unknown <ludovic.cottret@toulouse.inra.fr>
2019-09-26 14:17:04 +0200

    Add isCofactor setter and getter

--- met4j-io/src/main/java/fr/inra/toulouse/metexplore/met4j_io/annotations/reactant/ReactantAttributes.java
+++ met4j-io/src/main/java/fr/inra/toulouse/metexplore/met4j_io/annotations/reactant/ReactantAttributes.java
@@ -4,5 +4,27 @@ import fr.inra.toulouse.metexplore.met4j_core.biodata.BioReactant;
 import fr.inra.toulouse.metexplore.met4j_io.annotations.GenericAttributes;
 
 public class ReactantAttributes extends GenericAttributes {
+	public static final String IS_COFACTOR = "is_cofactor";
 	
+	/**
+	 * get is cofactor attribute
+	 * @param reactant
+	 * @return
+	 */
+	public static Boolean getIsCofactor(BioReactant reactant) {
+		if(reactant.getAttribute(IS_COFACTOR) == null) {
+			return false;
+		}
+		return (Boolean) reactant.getAttribute(IS_COFACTOR);
+
+	}
+	
+	/**
+	 * Set IsCofactor value
+	 * @param reactant
+	 * @param b
+	 */
+	public static void setIsCofactor(BioReactant reactant, boolean b) {
+		reactant.setAttribute(IS_COFACTOR, b);
+	}
 }

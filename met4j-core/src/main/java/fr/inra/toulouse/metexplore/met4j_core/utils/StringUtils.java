08bc7f713361f483bd568a1a079c591e82c0afbd
Unknown <ludovic.cottret@toulouse.inra.fr>
2019-10-09 14:51:11 +0200

    IsDouble and IsInteger methods

--- met4j-core/src/main/java/fr/inra/toulouse/metexplore/met4j_core/utils/StringUtils.java
+++ met4j-core/src/main/java/fr/inra/toulouse/metexplore/met4j_core/utils/StringUtils.java
@@ -10,4 +10,34 @@ public class StringUtils {
 		if(in.equalsIgnoreCase("null")) return true;
 		return false;
 	}
+
+	/**
+	 * From https://stackoverflow.com/questions/5439529/determine-if-a-string-is-an-integer-in-java/5439632#5439632
+	 * @param s
+	 * @return
+	 */
+	public static boolean isInteger(String s) {
+		try {
+			Integer.parseInt(s);
+		} catch(NumberFormatException e) {
+			return false;
+		} catch(NullPointerException e) {
+			return false;
+		}
+		// only got here if we didn't return false
+		return true;
+	}
+
+	public static boolean isDouble(String s) {
+		try {
+			Double.parseDouble(s);
+		} catch(NumberFormatException e) {
+			return false;
+		} catch(NullPointerException e) {
+			return false;
+		}
+		// only got here if we didn't return false
+		return true;
+	}
+
 }

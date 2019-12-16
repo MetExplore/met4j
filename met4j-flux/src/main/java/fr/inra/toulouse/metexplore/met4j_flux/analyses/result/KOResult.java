6809263467dcb5e5af33758d68f31921d71bd827
Unknown <ludovic.cottret@toulouse.inra.fr>
2019-12-16 09:40:24 +0100

    If result value = NaN, value = 0.0

--- met4j-flux/src/main/java/fr/inra/toulouse/metexplore/met4j_flux/analyses/result/KOResult.java
+++ met4j-flux/src/main/java/fr/inra/toulouse/metexplore/met4j_flux/analyses/result/KOResult.java
@@ -1,21 +1,21 @@
-/*******************************************************************************
+/**
  * Copyright INRA
- * 
- *  Contact: ludovic.cottret@toulouse.inra.fr
- * 
- * 
+ * <p>
+ * Contact: ludovic.cottret@toulouse.inra.fr
+ * <p>
+ * <p>
  * This software is governed by the CeCILL license under French law and
  * abiding by the rules of distribution of free software.  You can  use,
  * modify and/ or redistribute the software under the terms of the CeCILL
  * license as circulated by CEA, CNRS and INRIA at the following URL
  * "http://www.cecill.info".
- * 
+ * <p>
  * As a counterpart to the access to the source code and  rights to copy,
  * modify and redistribute granted by the license, users are provided only
  * with a limited warranty  and the software's author,  the holder of the
  * economic rights,  and the successive licensors  have only  limited
  * liability.
- *  In this respect, the user's attention is drawn to the risks associated
+ * In this respect, the user's attention is drawn to the risks associated
  * with loading,  using,  modifying and/or developing or reproducing the
  * software by the user in light of its specific status of free software,
  * that may mean  that it is complicated to manipulate,  and  that  also
@@ -25,9 +25,11 @@
  * requirements in conditions enabling the security of their systems and/or
  * data to be ensured and,  more generally, to use and operate it in the
  * same conditions as regards security.
- *  The fact that you are presently reading this means that you have had
+ * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
- ******************************************************************************/
+ * <p>
+ * 15 mars 2013
+ */
 /**
  * 15 mars 2013 
  */
@@ -53,261 +55,262 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 /**
- * 
+ *
  * Class representing the result of a KO analysis.
- * 
+ *
  * @author lmarmiesse 15 mars 2013
- * 
+ *
  */
 public class KOResult extends AnalysisResult {
 
-	/**
-	 * Map containing the result for each knocked out entity.
-	 */
-	private Map<BioEntity, Double> map = new HashMap<BioEntity, Double>();
+    /**
+     * Map containing the result for each knocked out entity.
+     */
+    private Map<BioEntity, Double> map = new HashMap<BioEntity, Double>();
 
-	/**
-	 * Field to search for entities in the plot.
-	 */
-	private JTextField searchField;
+    /**
+     * Field to search for entities in the plot.
+     */
+    private JTextField searchField;
 
-	/**
-	 * Table with all results.
-	 */
-	private JTable resultTable;
+    /**
+     * Table with all results.
+     */
+    private JTable resultTable;
 
-	/**
-	 * Adds a value to the map.
-	 */
-	public synchronized void addLine(BioEntity entity, double value) {
+    /**
+     * Adds a value to the map.
+     */
+    public synchronized void addLine(BioEntity entity, double value) {
 
-		map.put(entity, value);
-	}
+        map.put(entity, value);
+    }
 
-	public void writeToFile(String path) {
-		try {
-			PrintWriter out = new PrintWriter(new File(path));
+    public void writeToFile(String path) {
+        try {
+            PrintWriter out = new PrintWriter(new File(path));
 
-			out.println("KO results : \n");
+            out.println("KO results : \n");
 
-			for (BioEntity entity : map.keySet()) {
+            for (BioEntity entity : map.keySet()) {
 
-				double plotVal = Vars.round(map.get(entity));
+                double plotVal = Vars.round(map.get(entity));
 
-				if (Double.isNaN(plotVal)) {
-					out.println(entity.getId() + "(Unfeasible) obj value : " + Vars.round(map.get(entity)));
-				} else {
-					out.println(entity.getId() + " obj value : " + Vars.round(map.get(entity)));
-				}
+                if (Double.isNaN(plotVal)) {
+                    out.println(entity.getId() + "(Unfeasible) obj value : " + Vars.round(map.get(entity)));
+                } else {
+                    out.println(entity.getId() + " obj value : " + Vars.round(map.get(entity)));
+                }
 
-			}
-			out.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+            }
+            out.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	public double getValueForEntity(BioEntity ent) {
-		return map.get(ent);
-	}
+    public double getValueForEntity(BioEntity ent) {
+        return map.get(ent);
+    }
 
-	public void plot() {
+    public void plot() {
 
-		resultTable = new JTable(0, 2);
+        resultTable = new JTable(0, 2);
 
-		String[] columnNames = { "Entity name", "Objective value" };
+        String[] columnNames = {"Entity name", "Objective value"};
 
-		Object[][] data = new Object[map.size()][columnNames.length];
+        Object[][] data = new Object[map.size()][columnNames.length];
 
-		int i = 0;
-		for (BioEntity ent : map.keySet()) {
+        int i = 0;
+        for (BioEntity ent : map.keySet()) {
 
-			double plotVal = Vars.round(map.get(ent));
+            double plotVal = Vars.round(map.get(ent));
 
-			if (Double.isNaN(plotVal)) {
-				data[i] = new Object[] { ent.getId() + "(Unfeasible)", 0.0 };
-			} else {
-				data[i] = new Object[] { ent.getId(), plotVal };
-			}
-			i++;
-		}
+            if (Double.isNaN(plotVal)) {
+                data[i] = new Object[]{ent.getId() + "(Unfeasible)", 0.0};
+            } else {
+                data[i] = new Object[]{ent.getId(), plotVal};
+            }
+            i++;
+        }
 
-		DefaultTableModel model = new MyTableModel(data, columnNames);
-		resultTable.setModel(model);
+        DefaultTableModel model = new MyTableModel(data, columnNames);
+        resultTable.setModel(model);
 
-		final TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(resultTable.getModel());
-		resultTable.setRowSorter(sorter);
+        final TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(resultTable.getModel());
+        resultTable.setRowSorter(sorter);
 
-		JPanel northPanel = new JPanel();
+        JPanel northPanel = new JPanel();
 
-		JPanel centerPanel = new JPanel();
-		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.PAGE_AXIS));
+        JPanel centerPanel = new JPanel();
+        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.PAGE_AXIS));
 
-		JPanel searchPanel = new JPanel(new FlowLayout());
+        JPanel searchPanel = new JPanel(new FlowLayout());
 
-		searchPanel.add(new JLabel("Search for an entity : "));
+        searchPanel.add(new JLabel("Search for an entity : "));
 
-		searchField = new JTextField(10);
-		searchField.getDocument().addDocumentListener(new DocumentListener() {
+        searchField = new JTextField(10);
+        searchField.getDocument().addDocumentListener(new DocumentListener() {
 
-			public void changedUpdate(DocumentEvent arg0) {
-				updateTable(sorter);
-			}
+            public void changedUpdate(DocumentEvent arg0) {
+                updateTable(sorter);
+            }
 
-			public void insertUpdate(DocumentEvent arg0) {
-				updateTable(sorter);
-			}
+            public void insertUpdate(DocumentEvent arg0) {
+                updateTable(sorter);
+            }
 
-			public void removeUpdate(DocumentEvent arg0) {
-				updateTable(sorter);
-			}
+            public void removeUpdate(DocumentEvent arg0) {
+                updateTable(sorter);
+            }
 
-		});
+        });
 
-		searchPanel.add(searchField);
+        searchPanel.add(searchField);
 
-		centerPanel.add(searchPanel);
-		centerPanel.add(new JScrollPane(resultTable));
+        centerPanel.add(searchPanel);
+        centerPanel.add(new JScrollPane(resultTable));
 
-		JFrame frame = new JFrame("KO results");
+        JFrame frame = new JFrame("KO results");
 
-		frame.add(northPanel, BorderLayout.PAGE_START);
-		frame.add(centerPanel, BorderLayout.CENTER);
-		frame.pack();
-		RefineryUtilities.centerFrameOnScreen(frame);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+        frame.add(northPanel, BorderLayout.PAGE_START);
+        frame.add(centerPanel, BorderLayout.CENTER);
+        frame.pack();
+        RefineryUtilities.centerFrameOnScreen(frame);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
-	}
+    }
 
-	/**
-	 * Updates the table when a search is made in the plot.
-	 */
-	private void updateTable(TableRowSorter<TableModel> sorter) {
-		String text = searchField.getText();
-		if (sorter.getModelRowCount() != 0 && text.length() != 0) {
-			// case insensitive
-			sorter.setRowFilter(RowFilter.regexFilter("(?i)" + Pattern.quote(text), 0));
-		}
+    /**
+     * Updates the table when a search is made in the plot.
+     */
+    private void updateTable(TableRowSorter<TableModel> sorter) {
+        String text = searchField.getText();
+        if (sorter.getModelRowCount() != 0 && text.length() != 0) {
+            // case insensitive
+            sorter.setRowFilter(RowFilter.regexFilter("(?i)" + Pattern.quote(text), 0));
+        }
 
-	}
+    }
 
-	public Map<BioEntity, Double> getMap() {
-		return map;
-	}
+    public Map<BioEntity, Double> getMap() {
+        return map;
+    }
 
-	/**
-	 * 
-	 * @return a hashMap of the essential entities
-	 */
-	public BioCollection<BioEntity> getEssentialEntities() {
+    /**
+     *
+     * @return a hashMap of the essential entities
+     */
+    public BioCollection<BioEntity> getEssentialEntities() {
 
-		BioCollection<BioEntity> essentialEntities = new BioCollection<BioEntity>();
+        BioCollection<BioEntity> essentialEntities = new BioCollection<BioEntity>();
 
-		for (BioEntity entity : map.keySet()) {
+        for (BioEntity entity : map.keySet()) {
 
-			Double value = Vars.round(map.get(entity));
-			if (value.isNaN() || value == 0)
-				essentialEntities.add(entity);
-		}
-		return essentialEntities;
+            Double value = Vars.round(map.get(entity));
 
-	}
+            if (value.isNaN() || value == 0)
+                essentialEntities.add(entity);
+        }
+        return essentialEntities;
 
-	/**
-	 * 
-	 * @return a hashMap of the optima essential genes a gene is optima essential if
-	 *         its ko only decreases the objective optimal value
-	 */
-	public HashMap<String, BioEntity> getOptimaEntities(Double optimalValue) {
+    }
 
-		HashMap<String, BioEntity> optimaEntities = new HashMap<String, BioEntity>();
+    /**
+     *
+     * @return a hashMap of the optima essential genes a gene is optima essential if
+     *         its ko only decreases the objective optimal value
+     */
+    public HashMap<String, BioEntity> getOptimaEntities(Double optimalValue) {
 
-		for (BioEntity entity : map.keySet()) {
+        HashMap<String, BioEntity> optimaEntities = new HashMap<String, BioEntity>();
 
-			Double value = Vars.round(map.get(entity));
+        for (BioEntity entity : map.keySet()) {
 
-			if (!value.isNaN()) {
+            Double value = Vars.round(map.get(entity));
 
-				optimalValue = Vars.round(optimalValue);
+            if (!value.isNaN()) {
 
-				Double diff = Math.abs(optimalValue) - Math.abs(value);
+                optimalValue = Vars.round(optimalValue);
 
-				if (diff > 0 && value != 0) {
-					optimaEntities.put(entity.getId(), entity);
-				}
-			}
+                Double diff = Math.abs(optimalValue) - Math.abs(value);
 
-		}
-		return optimaEntities;
+                if (diff > 0 && value != 0) {
+                    optimaEntities.put(entity.getId(), entity);
+                }
+            }
 
-	}
+        }
+        return optimaEntities;
 
-	/**
-	 * 
-	 * @return a hashMap of the neutral genes a gene is neutral if its ko doesn't
-	 *         decrease the objective optimal value
-	 */
-	public HashMap<String, BioEntity> getNeutralEntities(Double optimalValue) {
+    }
 
-		HashMap<String, BioEntity> neutralEntities = new HashMap<String, BioEntity>();
+    /**
+     *
+     * @return a hashMap of the neutral genes a gene is neutral if its ko doesn't
+     *         decrease the objective optimal value
+     */
+    public HashMap<String, BioEntity> getNeutralEntities(Double optimalValue) {
 
-		for (BioEntity entity : map.keySet()) {
+        HashMap<String, BioEntity> neutralEntities = new HashMap<String, BioEntity>();
 
-			Double value = Vars.round(map.get(entity));
+        for (BioEntity entity : map.keySet()) {
 
-			if (!value.isNaN()) {
+            Double value = Vars.round(map.get(entity));
 
-				optimalValue = Vars.round(optimalValue);
+            if (!value.isNaN()) {
 
-				Double diff = Math.abs(optimalValue) - Math.abs(value);
+                optimalValue = Vars.round(optimalValue);
 
-				if (diff == 0) {
-					neutralEntities.put(entity.getId(), entity);
-				}
-			}
-		}
-		return neutralEntities;
+                Double diff = Math.abs(optimalValue) - Math.abs(value);
 
-	}
+                if (diff == 0) {
+                    neutralEntities.put(entity.getId(), entity);
+                }
+            }
+        }
+        return neutralEntities;
 
-	@Override
-	public void writeHTML(String path) {
-		try {
-			PrintWriter out = new PrintWriter(new File(path));
+    }
 
-			out.println("<table>");
+    @Override
+    public void writeHTML(String path) {
+        try {
+            PrintWriter out = new PrintWriter(new File(path));
 
-			out.println("<tr>");
-			out.println("<th>Entity name</th>");
-			out.println("<th>Objective value</th>");
-			out.println("</tr>");
+            out.println("<table>");
 
-			for (BioEntity entity : map.keySet()) {
-				out.println("<tr>");
+            out.println("<tr>");
+            out.println("<th>Entity name</th>");
+            out.println("<th>Objective value</th>");
+            out.println("</tr>");
 
-				double plotVal = Vars.round(map.get(entity));
+            for (BioEntity entity : map.keySet()) {
+                out.println("<tr>");
 
-				if (Double.isNaN(plotVal)) {
-					out.println("<td>" + entity.getId() + "(Unfeasible)</td>");
-					out.println("<td>" + Vars.round(map.get(entity)) + "</td>");
+                double plotVal = Vars.round(map.get(entity));
 
-				} else {
-					out.println("<td>" + entity.getId() + "</td>");
-					out.println("<td>" + Vars.round(map.get(entity)) + "</td>");
-				}
+                if (Double.isNaN(plotVal)) {
+                    out.println("<td>" + entity.getId() + "(Unfeasible)</td>");
+                    out.println("<td>" + Vars.round(map.get(entity)) + "</td>");
 
-				out.println("</tr>");
-			}
+                } else {
+                    out.println("<td>" + entity.getId() + "</td>");
+                    out.println("<td>" + Vars.round(map.get(entity)) + "</td>");
+                }
 
-			out.println("</table>");
-			out.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+                out.println("</tr>");
+            }
 
-	}
+            out.println("</table>");
+            out.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
 
 }
--- met4j-flux/src/main/java/fr/inra/toulouse/metexplore/met4j_flux/thread/ThreadKO.java
+++ met4j-flux/src/main/java/fr/inra/toulouse/metexplore/met4j_flux/thread/ThreadKO.java
@@ -1,21 +1,21 @@
-/*******************************************************************************
+/**
  * Copyright INRA
- * 
- *  Contact: ludovic.cottret@toulouse.inra.fr
- * 
- * 
+ * <p>
+ * Contact: ludovic.cottret@toulouse.inra.fr
+ * <p>
+ * <p>
  * This software is governed by the CeCILL license under French law and
  * abiding by the rules of distribution of free software.  You can  use,
  * modify and/ or redistribute the software under the terms of the CeCILL
  * license as circulated by CEA, CNRS and INRIA at the following URL
  * "http://www.cecill.info".
- * 
+ * <p>
  * As a counterpart to the access to the source code and  rights to copy,
  * modify and redistribute granted by the license, users are provided only
  * with a limited warranty  and the software's author,  the holder of the
  * economic rights,  and the successive licensors  have only  limited
  * liability.
- *  In this respect, the user's attention is drawn to the risks associated
+ * In this respect, the user's attention is drawn to the risks associated
  * with loading,  using,  modifying and/or developing or reproducing the
  * software by the user in light of its specific status of free software,
  * that may mean  that it is complicated to manipulate,  and  that  also
@@ -25,9 +25,11 @@
  * requirements in conditions enabling the security of their systems and/or
  * data to be ensured and,  more generally, to use and operate it in the
  * same conditions as regards security.
- *  The fact that you are presently reading this means that you have had
+ * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
- ******************************************************************************/
+ * <p>
+ * 15 mars 2013
+ */
 /**
  * 15 mars 2013 
  */
@@ -47,123 +49,120 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Thread to perform an FVA analysis.
- * 
+ *
  * @author lmarmiesse 15 mars 2013
- * 
+ *
  */
 public class ThreadKO extends ResolveThread {
 
-	/**
-	 * Number of entities to treat.
-	 */
-	private double todo;
+    /**
+     *
+     * For the output Thread safe integer
+     *
+     */
+    private static AtomicInteger nbPrintedStars = new AtomicInteger(0);
+    protected BioCollection<BioEntity> entitiesInInteractionNetwork = new BioCollection<BioEntity>();
+    protected List<Constraint> interactionNetworkConstraints = new ArrayList<Constraint>();
+    /**
+     * Number of entities to treat.
+     */
+    private double todo;
+    /**
+     * Contains all entities to treat.
+     */
+    private Queue<BioEntity> entities;
+    /**
+     * The KO result.
+     */
+    private KOResult result;
 
-	/**
-	 * Contains all entities to treat.
-	 */
-	private Queue<BioEntity> entities;
-
-	/**
-	 * The KO result.
-	 */
-	private KOResult result;
-
-	/**
-	 * 
-	 * For the output Thread safe integer
-	 * 
-	 */
-	private static AtomicInteger nbPrintedStars = new AtomicInteger(0);
-
-	protected BioCollection<BioEntity> entitiesInInteractionNetwork = new BioCollection<BioEntity>();
-
-	protected List<Constraint> interactionNetworkConstraints = new ArrayList<Constraint>();
-
-	public ThreadKO(Bind b, Queue<BioEntity> entities, KOResult result, Objective obj,
+    public ThreadKO(Bind b, Queue<BioEntity> entities, KOResult result, Objective obj,
                     BioCollection<BioEntity> entitiesInInteractionNetwork, List<Constraint> interactionNetwotkConstraints) {
-		super(b, obj);
-		this.todo = entities.size();
-		this.entities = entities;
-		this.result = result;
-		this.entitiesInInteractionNetwork = entitiesInInteractionNetwork;
-		this.interactionNetworkConstraints = interactionNetwotkConstraints;
-		
-		nbPrintedStars = new AtomicInteger(0);
+        super(b, obj);
+        this.todo = entities.size();
+        this.entities = entities;
+        this.result = result;
+        this.entitiesInInteractionNetwork = entitiesInInteractionNetwork;
+        this.interactionNetworkConstraints = interactionNetwotkConstraints;
 
-	}
+        nbPrintedStars = new AtomicInteger(0);
 
-	public void run() {
+    }
 
-		BioEntity entity;
+    public void run() {
 
-		while ((entity = entities.poll()) != null) {
+        BioEntity entity;
 
-			Map<BioEntity, Double> entityMap = new HashMap<BioEntity, Double>();
-			entityMap.put(entity, 1.0);
+        while ((entity = entities.poll()) != null) {
 
-			boolean removedConst = false;
-			Constraint toRemove = null;
-			// we remove a constraint that is already present on this entity
-			if (bind.getSimpleConstraints().containsKey(entity)) {
-				removedConst = true;
+            Map<BioEntity, Double> entityMap = new HashMap<BioEntity, Double>();
+            entityMap.put(entity, 1.0);
 
-				toRemove = bind.getSimpleConstraints().get(entity);
-				bind.getSimpleConstraints().remove(entity);
-				bind.getConstraints().remove(toRemove);
+            boolean removedConst = false;
+            Constraint toRemove = null;
+            // we remove a constraint that is already present on this entity
+            if (bind.getSimpleConstraints().containsKey(entity)) {
+                removedConst = true;
+
+                toRemove = bind.getSimpleConstraints().get(entity);
+                bind.getSimpleConstraints().remove(entity);
+                bind.getConstraints().remove(toRemove);
 //				bind.prepareSolver();
-			}
+            }
 
-			//
+            //
 
-			List<Constraint> constraintsToAdd = new ArrayList<Constraint>();
+            List<Constraint> constraintsToAdd = new ArrayList<Constraint>();
 
-			Constraint newConstraint = new Constraint(entityMap, 0.0, 0.0);
+            Constraint newConstraint = new Constraint(entityMap, 0.0, 0.0);
 
-			constraintsToAdd.add(newConstraint);
+            constraintsToAdd.add(newConstraint);
 
-			// if the entity is not in the interaction network, we
-			// don't recompute the attractors
-			if (!entitiesInInteractionNetwork.contains(entity)) {
-				constraintsToAdd.addAll(interactionNetworkConstraints);
-				 bind.checkInteractionNetwork = false;
-			} else {
-				bind.checkInteractionNetwork = true;
-				
-			}
-			
+            // if the entity is not in the interaction network, we
+            // don't recompute the attractors
+            if (!entitiesInInteractionNetwork.contains(entity)) {
+                constraintsToAdd.addAll(interactionNetworkConstraints);
+                bind.checkInteractionNetwork = false;
+            } else {
+                bind.checkInteractionNetwork = true;
 
-			List<Constraint> tmpConstraints = new ArrayList<Constraint>(constraintsToAdd);
-			
-			for(Constraint c : tmpConstraints) 
-			{
-				if(c.getEntityNames().containsKey(entity.getId())) {
-					constraintsToAdd.remove(c);
-				}
-			}
-			
-			constraintsToAdd.add(newConstraint);
+            }
 
-			DoubleResult value = bind.FBA(constraintsToAdd, false, true);
 
-			result.addLine(entity, value.result);
+            List<Constraint> tmpConstraints = new ArrayList<Constraint>(constraintsToAdd);
 
-			if (Vars.verbose) {
-				int percent = (int) Math.round((todo - entities.size()) / todo * 100);
+            for (Constraint c : tmpConstraints) {
+                if (c.getEntityNames().containsKey(entity.getId())) {
+                    constraintsToAdd.remove(c);
+                }
+            }
 
-				while (nbPrintedStars.intValue() < (percent / 2)) {
-					System.err.print("*");
-					nbPrintedStars.incrementAndGet();
-				}
-			}
+            constraintsToAdd.add(newConstraint);
 
-			if (removedConst) {
-				bind.getConstraints().add(toRemove);
-				bind.getSimpleConstraints().put(entity, toRemove);
-			}
+            DoubleResult value = bind.FBA(constraintsToAdd, false, true);
 
-		}
+            if (Double.isNaN(value.result)) {
+                value.result = 0.0;
+            }
+            result.addLine(entity, value.result);
+
+            if (Vars.verbose) {
+                int percent = (int) Math.round((todo - entities.size()) / todo * 100);
+
+                while (nbPrintedStars.intValue() < (percent / 2)) {
+                    System.err.print("*");
+                    nbPrintedStars.incrementAndGet();
+                }
+            }
+
+            if (removedConst) {
+                bind.getConstraints().add(toRemove);
+                bind.getSimpleConstraints().put(entity, toRemove);
+            }
+
+        }
 
 //		bind.end();
-		
-	}
+
+    }
 }

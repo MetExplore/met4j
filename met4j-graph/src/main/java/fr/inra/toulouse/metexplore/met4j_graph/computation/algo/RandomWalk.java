23672a01e8d6ced17d7b64dbdec3bbad4fb175d6
Unknown <ludovic.cottret@toulouse.inra.fr>
2020-01-17 18:17:00 +0100

    Change license text in each java file

--- met4j-graph/src/main/java/fr/inra/toulouse/metexplore/met4j_graph/computation/algo/RandomWalk.java
+++ met4j-graph/src/main/java/fr/inra/toulouse/metexplore/met4j_graph/computation/algo/RandomWalk.java
@@ -1,21 +1,24 @@
-/*******************************************************************************
- * Copyright INRA
- * 
- *  Contact: ludovic.cottret@toulouse.inra.fr
- * 
- * 
+/*
+ * Copyright INRAE (2020)
+ *
+ * contact-metexplore@inrae.fr
+ *
+ * This software is a computer program whose purpose is to [describe
+ * functionalities and technical features of your software].
+ *
  * This software is governed by the CeCILL license under French law and
  * abiding by the rules of distribution of free software.  You can  use,
  * modify and/ or redistribute the software under the terms of the CeCILL
  * license as circulated by CEA, CNRS and INRIA at the following URL
- * "http://www.cecill.info".
- * 
+ * "https://cecill.info/licences/Licence_CeCILL_V2.1-en.html".
+ *
  * As a counterpart to the access to the source code and  rights to copy,
  * modify and redistribute granted by the license, users are provided only
  * with a limited warranty  and the software's author,  the holder of the
  * economic rights,  and the successive licensors  have only  limited
  * liability.
- *  In this respect, the user's attention is drawn to the risks associated
+ *
+ * In this respect, the user's attention is drawn to the risks associated
  * with loading,  using,  modifying and/or developing or reproducing the
  * software by the user in light of its specific status of free software,
  * that may mean  that it is complicated to manipulate,  and  that  also
@@ -25,9 +28,11 @@
  * requirements in conditions enabling the security of their systems and/or
  * data to be ensured and,  more generally, to use and operate it in the
  * same conditions as regards security.
- *  The fact that you are presently reading this means that you have had
+ *
+ * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
- ******************************************************************************/
+ *
+ */
 package fr.inra.toulouse.metexplore.met4j_graph.computation.algo;
 
 import java.util.HashMap;

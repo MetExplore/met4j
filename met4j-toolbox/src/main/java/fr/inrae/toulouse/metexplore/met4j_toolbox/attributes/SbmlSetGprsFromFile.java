c5999503c9007463a2acd1c69e7394b8bbfb0952
lcottret <ludovic.cottret@inrae.fr>
2020-03-09 10:40:54 +0100

    new class & app to set gprs from file

---
+++ met4j-toolbox/src/main/java/fr/inrae/toulouse/metexplore/met4j_toolbox/attributes/SbmlSetGprsFromFile.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright INRAE (2020)
+ *
+ * contact-metexplore@inrae.fr
+ *
+ * This software is a computer program whose purpose is to [describe
+ * functionalities and technical features of your software].
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "https://cecill.info/licences/Licence_CeCILL_V2.1-en.html".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ *
+ */
+
+package fr.inrae.toulouse.metexplore.met4j_toolbox.attributes;
+
+import fr.inrae.toulouse.metexplore.met4j_core.biodata.BioNetwork;
+import fr.inrae.toulouse.metexplore.met4j_io.jsbml.reader.JsbmlReader;
+import fr.inrae.toulouse.metexplore.met4j_io.jsbml.reader.Met4jSbmlReaderException;
+import fr.inrae.toulouse.metexplore.met4j_io.jsbml.writer.JsbmlWriter;
+import fr.inrae.toulouse.metexplore.met4j_io.tabulated.attributes.SetGprsFromFile;
+import fr.inrae.toulouse.metexplore.met4j_toolbox.generic.Met4jApplication;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import java.io.IOException;
+
+public class SbmlSetGprsFromFile  extends AbstractSbmlSetReaction {
+
+
+    @Option(name="-cgpr", usage="[2] number of the column where are the gprs")
+    private int colgpr=2;
+
+    @Override
+    public String getLabel() {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    public String getDescription() {
+        return "Create a new SBML file from an original sbml file and a tabulated file containing reaction ids and gprs written in a cobra way";
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        SbmlSetGprsFromFile s = new SbmlSetGprsFromFile();
+
+        s.parseArguments(args);
+
+        s.run();
+    }
+
+    private void run() {
+
+
+        JsbmlReader reader = null;
+        try {
+            reader = new JsbmlReader(this.sbml);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Unable to read the sbml file "+this.sbml);
+            System.exit(0);
+        }
+
+        BioNetwork bn = null;
+        try {
+            bn = reader.read();
+        } catch (Met4jSbmlReaderException e) {
+            e.printStackTrace();
+            System.err.println("Problem while reading the sbml file "+this.sbml);
+            System.exit(0);
+        }
+
+        SetGprsFromFile sgff = new SetGprsFromFile(this.colid-1, this.colgpr-1, bn, this.tab, this.c, this.nSkip, this.p, false);
+
+        Boolean flag = true;
+
+        try {
+            flag = sgff.setAttributes();
+        } catch (Exception e) {
+            flag=false;
+        }
+
+        if(!flag) {
+            System.err.println("Error in setting gene associations");
+            System.exit(0);
+        }
+
+        JsbmlWriter writer = new JsbmlWriter(this.out, bn);
+
+        try {
+            writer.write();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Error in writing the sbml file");
+            flag=false;
+            System.exit(0);
+        }
+
+        System.exit(1);
+
+    }
+
+
+}

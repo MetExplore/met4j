c2b327ee266e5eff989c5214863a1031e1deff2d
lcottret <ludovic.cottret@inrae.fr>
2020-03-09 16:04:33 +0100

    class/app to set charges from file

---
+++ met4j-io/src/main/java/fr/inrae/toulouse/metexplore/met4j_io/tabulated/attributes/SetChargesFromFile.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright INRAE (2020)
+ *
+ * contact-metexplore@inrae.fr
+ *
+ * This software is a computer program whose purpose is to [describe
+ * functionalities and technical features of your software].
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "https://cecill.info/licences/Licence_CeCILL_V2.1-en.html".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ *
+ */
+
+package fr.inrae.toulouse.metexplore.met4j_io.tabulated.attributes;
+
+import fr.inrae.toulouse.metexplore.met4j_core.biodata.BioNetwork;
+import fr.inrae.toulouse.metexplore.met4j_core.utils.StringUtils;
+
+import java.io.IOException;
+
+public class SetChargesFromFile extends AbstractSetAttributesFromFile {
+
+    public SetChargesFromFile(int colId, int colAttr, BioNetwork bn, String fileIn, String c, int nSkip, Boolean p, Boolean s) {
+
+        super(colId, colAttr, bn, fileIn, c, nSkip, "M", p, s);
+
+    }
+
+    /**
+     * Test the charge
+     */
+    public Boolean testAttribute(String charge) {
+        if(StringUtils.isInteger(charge)) {
+            return true;
+        }
+        else {
+            System.err.println(charge+ " no in good format (integer)");
+            return false;
+        }
+
+    }
+
+    /**
+     * Reads the file and sets the attributes
+     * @return
+     * @throws IOException
+     */
+    public Boolean setAttributes() throws IOException {
+
+        Boolean flag = true;
+
+        try {
+            flag = this.test();
+        } catch (IOException e) {
+            return false;
+        }
+
+        if(!flag) {
+            return false;
+        }
+
+        int n = 0;
+
+        for(String id : this.getIdAttributeMap().keySet()) {
+
+            n++;
+
+            String charge = this.getIdAttributeMap().get(id);
+
+            this.getNetwork().getMetabolitesView().get(id).setCharge(Integer.parseInt(charge));
+
+        }
+
+
+        System.err.println(n+" attributions");
+
+        return flag;
+
+    }
+
+
+}

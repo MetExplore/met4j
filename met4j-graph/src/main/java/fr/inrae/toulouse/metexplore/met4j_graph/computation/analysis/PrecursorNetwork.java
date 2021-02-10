83a35679d86f1c271cfab65f12a4d4d1471c2d76
cfrainay <clement.frainay@inra.fr>
2021-02-10 19:13:08 +0100

    [feature] add precursor network computation
    from a list of compound to produce and ubiquitous compound, create a network of all upstream reactions. PrecursorSet can be extracted from computed network using static methods.

---
+++ met4j-graph/src/main/java/fr/inrae/toulouse/metexplore/met4j_graph/computation/analysis/PrecursorNetwork.java
@@ -0,0 +1,163 @@
+package fr.inrae.toulouse.metexplore.met4j_graph.computation.analysis;
+
+import fr.inrae.toulouse.metexplore.met4j_core.biodata.BioEntity;
+import fr.inrae.toulouse.metexplore.met4j_core.biodata.BioMetabolite;
+import fr.inrae.toulouse.metexplore.met4j_core.biodata.BioReaction;
+import fr.inrae.toulouse.metexplore.met4j_core.biodata.collection.BioCollection;
+import fr.inrae.toulouse.metexplore.met4j_graph.core.bipartite.BipartiteGraph;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PrecursorNetwork {
+
+    /**
+     * The graph
+     */
+    private final BipartiteGraph g;
+
+    /**
+     * The available compounds.
+     */
+    private BioCollection<BioMetabolite> availableCpds;
+
+    /**
+     * The compounds to reach (optional)
+     */
+    private BioCollection<BioMetabolite> cpdToProduce;
+
+    /**
+     *  add by-products, compounds produced by reactions of the precursor set whitout being a precursor of the compounds to produce.
+     */
+    private boolean addByProducts = false; //TODO
+
+    /**
+     * Instantiates a new precursor class
+     *
+     * @param bipartiteGraph  the bipartite graph
+     * @param availableCpds   the available compounds (ubiquitous compounds like Water and ATP)
+     * @param cpdToProduce      The compounds to reach (optional)
+     * @param reactionToAvoid the reactions to avoid
+     */
+    public PrecursorNetwork(BipartiteGraph bipartiteGraph, BioCollection<BioMetabolite> availableCpds, BioCollection<BioMetabolite> cpdToProduce, BioCollection<BioReaction> reactionToAvoid) {
+        this.g = bipartiteGraph;
+        this.availableCpds = availableCpds;
+        this.cpdToProduce = cpdToProduce;
+        g.removeAllVertices(reactionToAvoid);
+    }
+
+    /**
+     * Instantiates a new precursor class.
+     *
+     * @param bipartiteGraph  the bipartite graph
+     * @param availableCpds   the available compounds (ubiquitous compounds like Water and ATP)
+     */
+    public PrecursorNetwork(BipartiteGraph bipartiteGraph, BioCollection<BioMetabolite> availableCpds, BioCollection<BioMetabolite> cpdToProduce) {
+        this.g = bipartiteGraph;
+        this.availableCpds = availableCpds;
+        this.cpdToProduce = cpdToProduce;
+    }
+
+
+    /**
+     * Gets the precursor network: all the upstream reactions that leads to the production of a set of compounds
+     *
+     * @return the precursor network
+     */
+    public BipartiteGraph getPrecursorNetwork() throws IllegalArgumentException {
+
+        //check network consistency
+        if (!this.g.isConsistent())
+            throw new IllegalArgumentException("The network structure must be consistent with reaction reactant lists");
+
+        BipartiteGraph precusorNetwork = new BipartiteGraph();
+
+        HashSet<BioMetabolite> visited = new HashSet<>(availableCpds);
+        Queue<BioMetabolite> metabolitesToProduce = new LinkedList<>(cpdToProduce);
+
+        //Go until their is no required precursor to produce.
+        while (!metabolitesToProduce.isEmpty()) {
+
+            BioMetabolite m = metabolitesToProduce.remove();
+
+            //add compound to network
+            precusorNetwork.addVertex(m);
+
+            for(BioEntity p : g.predecessorListOf(m)) {
+                BioReaction r = (BioReaction) p;
+
+                if (!precusorNetwork.containsVertex(r)) {
+
+                    //add producing reaction to network
+                    precusorNetwork.addVertex(r);
+
+
+                    if (!r.isReversible()) {
+                        //add substrate of producing reaction to queue, if not already visited.
+                        for (BioEntity p2 : g.predecessorListOf(r)) {
+                            BioMetabolite s = (BioMetabolite) p2;
+                            if (!visited.contains(s) && !metabolitesToProduce.contains(s)) {
+                                metabolitesToProduce.add(s);
+                            }
+                        }
+                    }else{
+                        if(r.getLeftsView().contains(m)){
+                            for (BioMetabolite s : r.getRightsView()) {
+                                if (!visited.contains(s) && !metabolitesToProduce.contains(s)) {
+                                    metabolitesToProduce.add(s);
+                                }
+                            }
+                        }else{
+                            for (BioMetabolite s : r.getLeftsView()) {
+                                if (!visited.contains(s) && !metabolitesToProduce.contains(s)) {
+                                    metabolitesToProduce.add(s);
+                                }
+                            }
+                        }
+                    }
+                }
+                precusorNetwork.addEdge(r, m, g.getEdge(r, m));
+            }
+            visited.add(m);
+        }
+
+
+        return precusorNetwork;
+    }
+
+    /**
+     * get precursor set from a precursor network.
+     * compounds not produced or only produced by exchange reactions are considered precursors.
+     * @param precusorNetwork
+     * @return
+     */
+    public static BioCollection<BioMetabolite> getPrecursors(BipartiteGraph precusorNetwork){
+        BioCollection<BioMetabolite> precursor = new BioCollection<>();
+        BioCollection<BioReaction> exchange = PrecursorNetwork.getExchangeReactions(precusorNetwork);
+        for(BioMetabolite v : precusorNetwork.compoundVertexSet()){
+            if(precusorNetwork.inDegreeOf(v)==0){
+                precursor.add(v);
+            }else{
+                if(exchange.containsAll(precusorNetwork.predecessorListOf(v))){
+                    precursor.add(v);
+                }
+            }
+        }
+        return precursor;
+    }
+
+    /**
+     * get incoming exchange reactions in precursor network. (reaction without substrate)
+     * @param precusorNetwork
+     * @return
+     */
+    public static BioCollection<BioReaction> getExchangeReactions(BipartiteGraph precusorNetwork){
+        BioCollection<BioReaction> precursor = new BioCollection<>();
+        for(BioReaction v : precusorNetwork.reactionVertexSet()){
+            if(precusorNetwork.inDegreeOf(v)==0) precursor.add(v);
+        }
+        return precursor;
+    }
+
+}

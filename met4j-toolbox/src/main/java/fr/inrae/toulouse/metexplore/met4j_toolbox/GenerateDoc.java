89c3ab64c97ac38303b20839db62290ea17eb960
cfrainay <clement.frainay@inrae.fr>
2022-01-24 16:41:35 +0100

    Create alternative Main app that generate pretty html table

---
+++ met4j-toolbox/src/main/java/fr/inrae/toulouse/metexplore/met4j_toolbox/GenerateDoc.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright INRAE (2020)
+ *
+ * contact-metexplore@inrae.fr
+ *
+ * This software is a computer program whose purpose is to [describe
+ * functionalities and technical features of your software].
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "https://cecill.info/licences/Licence_CeCILL_V2.1-en.html".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ *
+ */
+package fr.inrae.toulouse.metexplore.met4j_toolbox;
+
+import fr.inrae.toulouse.metexplore.met4j_toolbox.utils.ResourceURLFilter;
+import fr.inrae.toulouse.metexplore.met4j_toolbox.utils.Resources;
+
+import java.lang.reflect.Constructor;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * generate doc by creating a html table with name and description of each app
+ *
+ * @author cfrainay
+ * @version $Id: $Id
+ */
+public class GenerateDoc {
+
+    /**
+     * <p>main.</p>
+     *
+     * @param args an array of {@link java.lang.String} objects.
+     */
+    public static void main(String[] args) {
+
+        try {
+            ResourceURLFilter filter = new ResourceURLFilter() {
+
+                public boolean accept(URL u) {
+
+                    String path = Main.class.getPackage().getName()
+                            .replace(".", "/");
+
+                    String s = u.getFile();
+                    return s.endsWith(".class") && !s.contains("$")
+                            && s.contains(path);
+                }
+            };
+
+            String path = Main.class.getPackage().getName().replace(".", "/");
+            HashMap<String, Set<String>> packages = new HashMap<>();
+            HashMap<String, String> descs = new HashMap<>();
+
+            for (URL u : Resources.getResourceURLs(Resources.class, filter)) {
+
+                String entry = u.getFile();
+
+                int idx = entry.indexOf(path);
+
+                entry = entry
+                        .substring(idx, entry.length() - ".class".length());
+
+                Class<?> myClass = Class.forName(entry.replace('/', '.'));
+
+
+                try {
+                    myClass.getMethod("main", String[].class);
+                    Constructor<?> ctor = myClass.getConstructor();
+
+                    Object obj = ctor.newInstance();
+
+                    String packageName = myClass.getPackageName();
+                    String id = myClass.getCanonicalName();
+
+                    if (!packages.containsKey(packageName)) {
+                        packages.put(packageName, new HashSet<>());
+                    }
+                    String desc = "";
+                    try {
+                        Object labelo = myClass.getMethod("getLabel").invoke(obj);
+                        String label = labelo !=null ? (String) labelo : "";
+                        Object spo = myClass.getMethod("getShortDescription").invoke(obj);
+                        String sp = spo !=null ? (String) spo : "";
+                        sp=sp.replaceAll("\n","<br/>");
+                        Object lpo = myClass.getMethod("getLongDescription").invoke(obj);
+                        String lp = lpo !=null ? (String) lpo : "";
+                        lp=lp.replaceAll("\n\r?","<br/>");
+                        desc += "<tr><td>"+
+                                label +
+                                "</td><td>" +
+                                sp+"<details><summary><small>more</small></summary>" +
+                                lp+"</details>"+
+                                "</td></tr>";
+
+                        descs.put(id, desc);
+                        packages.get(packageName).add(id);
+                    } catch (Exception e) {
+                        desc += "No description !";
+                    }
+
+
+                } catch (Exception e1) {
+                }
+
+            }
+
+            System.out.println("# Met4j-Toolbox\n" +
+                    "The applications are classified by package.\n"+
+                    "The complete class name must be provided " +
+                    "(e.g. fr.inrae.toulouse.metexplore.met4j_toolbox.attributes.SbmlSetChargesFromFile) to launch the app\n" +
+                    "Launch the application with the -h parameter to get the list of the parameters and a complete description.\n");
+            packages.keySet().stream().sorted().forEach(packageName -> {
+                Set<String> classes = packages.get(packageName);
+                if (classes.size() > 0) {
+                    System.out.println("<table>");
+                    System.out.println("<thead>" +
+                            "<tr>" +
+                            "<th colspan=\"2\">"+
+                            "Package " + packageName +
+                            "</th>" +
+                            "</tr>" +
+                            "</thead>");
+                    System.out.println("<tbody>");
+                    classes.stream().sorted().forEach(className -> {
+                        String desc = descs.get(className);
+                        System.out.println(desc);
+                    });
+                    System.out.println("</tbody>");
+                    System.out.println("</table>");
+                }
+
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}

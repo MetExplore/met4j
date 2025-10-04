a5e897e1d49c2b7cd3664e06556498fe24db315d
Clement Frainay <clement.frainay@inra.fr>
2025-10-04 15:27:13 +0200

    add extra test case for SideCompoundScan

--- met4j-toolbox/src/test/java/integration/SideCompoundsScanIT.java
+++ met4j-toolbox/src/test/java/integration/SideCompoundsScanIT.java
@@ -1,19 +1,17 @@
 package integration;
 
+import org.junit.Assert;
 import org.junit.Test;
 import utils.IThelper;
-import org.junit.Assert;
 
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SideCompoundsScanIT {
 
@@ -105,6 +103,56 @@ public class SideCompoundsScanIT {
 
         assertEquals(0, result.exitCode());
         assertTrue(Files.exists(actualOutput));
+    }
+
+    @Test
+    public void testSideCompoundsScan2() throws Exception {
+
+        Path actualOutput = null;
+        Path tmpDir = null;
+        try {
+            actualOutput = Files.createTempFile("test-output-SideCompoundsScan", ".tsv");
+            tmpDir = Files.createTempDirectory("test-input-SideCompoundsScan");
+            tmpDir.toFile().deleteOnExit();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+            Assert.fail("Creation of the temporary directory");
+        }
+
+        String inputPathFile =IThelper.copyProjectResource("XF_network.sbml",tmpDir);
+        IThelper.ProcessResult result = IThelper.runCli(
+                "fr.inrae.toulouse.metexplore.met4j_toolbox.networkAnalysis.SideCompoundsScan",
+                "-i",
+                inputPathFile,
+                "-id",
+                "-s",
+                "-m",
+                "by_name",
+                "-dp",
+                "2",
+                "-cc",
+                "-uf",
+                "-o",
+                actualOutput.toString()
+        );
+
+
+        assertEquals(0, result.exitCode());
+        assertTrue(Files.exists(actualOutput));
+
+        BufferedReader reader = new BufferedReader(new FileReader(actualOutput.toFile()));
+        String line = reader.readLine();
+        String sep = "\t";
+        int ncol = 0;
+        while (line != null) {
+            int l = line.split(sep).length;
+            if (l>ncol) ncol=l;
+            line = reader.readLine();
+        }
+        reader.close();
+        assertEquals(1,ncol);
+
+
     }
     
 }
